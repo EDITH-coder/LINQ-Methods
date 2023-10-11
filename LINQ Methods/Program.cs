46b4ca6587eb4e9adea0a101e810c08a07b9d605
EDITH_Coder <edithmashapo@gmail.com>
2023-10-11 05:04:59 +0200

    Add project files.

---
+++ LINQ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers= new int[7] {8,9,3,36,2,10,25};
+            Console.WriteLine("The minimum number is:" + Numbers.Min());
+            Console.WriteLine("The average is: " +Numbers.Average());
+            Console.WriteLine("The maximum number is: " +Numbers.Max());
+            Console.WriteLine("The sum is: " + Numbers.Sum());
+            Console.WriteLine("The first number is: " +Numbers.First());
+            Console.WriteLine("The last number is: " + Numbers.Last());
+           
+
+            Console.ReadLine();
+        }
+    }
+}
